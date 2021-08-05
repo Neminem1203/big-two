d16827cd886fe80cb757e19cf6a4471e66f06978
Paul <Neminem1203@users.noreply.github.com>
2021-08-05 07:45:06 -0400

    changed port of socket

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Login from "./components/login.js";
 import Error from "./components/errors.js";
 import Game from "./components/game.js";
 
-const socket = io("http://localhost:5001", {
+const socket = io(`http://localhost:${process.end.PORT+1}`, {
   transports: ["websocket", "polling"]
 });
 
